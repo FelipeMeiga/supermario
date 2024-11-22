b732531732682229dba0c6b799260acd96f15fef
FelipeMeiga <felipe.maia.meiga@gmail.com>
2024-11-22 15:30:26 -0300

    Refine camera movement

--- Camera.py
+++ Camera.py
@@ -12,10 +12,17 @@ class Camera:
         return entity.hitbox.move(-self.camera.left, -self.camera.top)
 
     def update(self, target):
-        x = target.rect.centerx - int(self.screen_width / 2)
-        y = target.rect.centery - int(self.screen_height / 2)
+        target_x = target.rect.centerx - int(self.screen_width / 2)
+        target_y = target.rect.centery - int(self.screen_height / 2)
+        
+        distance_x = target_x - self.camera.x
+        distance_y = target_y - self.camera.y
 
-        x = max(0, min(x, self.width - self.screen_width))
-        y = max(0, min(y, self.height - self.screen_height))
+        acceleration = 0.1
 
-        self.camera = pygame.Rect(x, y, self.width, self.height)
+        self.camera.x += int(distance_x * acceleration)
+        self.camera.y += int(distance_y * acceleration)
+
+        self.camera.x = max(0, min(self.camera.x, self.width - self.screen_width))
+        self.camera.y = max(0, min(self.camera.y, self.height - self.screen_height))
+
